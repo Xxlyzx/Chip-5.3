fd9fe809e843f7493d402d719a48219ebccd91ff
Yisha Tang <yisha_tang@berkeley.edu>
2026-07-23 08:22:47 +0000

    Add movie sorting support

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,13 +1,26 @@
 class Movie < ApplicationRecord
   RATINGS = %w[G PG PG-13 R].freeze
+  SORTABLE_COLUMNS = %w[title release_date].freeze
 
   def self.all_ratings
     RATINGS
   end
 
-  def self.with_ratings(ratings_list)
-    return all if ratings_list.blank?
+  def self.with_ratings(ratings_list, sort_by = "title")
+    movies =
+      if ratings_list.blank?
+        all
+      else
+        where(rating: ratings_list)
+      end
 
-    where(rating: ratings_list)
+    sort_column =
+      if SORTABLE_COLUMNS.include?(sort_by)
+        sort_by
+      else
+        "title"
+      end
+
+    movies.order(sort_column)
   end
 end
