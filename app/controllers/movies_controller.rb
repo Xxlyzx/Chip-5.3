cef82a6078c2dbe43c79b68ace2f44c1f40365c0
Yisha Tang <yisha_tang@berkeley.edu>
2026-07-23 09:10:29 +0000

    Remember movie filtering and sorting settings

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -5,21 +5,62 @@ class MoviesController < ApplicationController
   def index
     @all_ratings = Movie.all_ratings
 
-    @ratings_to_show =
-      if params[:ratings].present?
-        params[:ratings].keys
+    form_submitted = params[:settings_submitted] == "1"
+    new_ratings_supplied = params[:ratings].present?
+    new_sort_supplied = params[:sort_by].present?
+
+    settings_supplied =
+      form_submitted || new_ratings_supplied || new_sort_supplied
+
+    if settings_supplied
+      if new_ratings_supplied
+        raw_ratings = params[:ratings]
+
+        selected_ratings =
+          if raw_ratings.respond_to?(:keys)
+            raw_ratings.keys
+          else
+            Array(raw_ratings)
+          end
+
+        @ratings_to_show =
+          (selected_ratings & @all_ratings).presence || @all_ratings
+      elsif form_submitted
+        # An explicitly submitted form with no ratings means
+        # that every checkbox was unchecked: show all ratings.
+        @ratings_to_show = @all_ratings
       else
-        @all_ratings
+        saved_ratings = Array(session[:ratings]) & @all_ratings
+        @ratings_to_show = saved_ratings.presence || @all_ratings
       end
 
-    requested_sort = params[:sort_by]
+      requested_sort = params[:sort_by]
+      saved_sort = session[:sort_by]
 
-    @sort_by =
-      if Movie::SORTABLE_COLUMNS.include?(requested_sort)
-        requested_sort
-      else
-        "title"
-      end
+      @sort_by =
+        if Movie::SORTABLE_COLUMNS.include?(requested_sort)
+          requested_sort
+        elsif Movie::SORTABLE_COLUMNS.include?(saved_sort)
+          saved_sort
+        else
+          "title"
+        end
+
+      session[:ratings] = @ratings_to_show
+      session[:sort_by] = @sort_by
+    else
+      saved_ratings = Array(session[:ratings]) & @all_ratings
+      @ratings_to_show = saved_ratings.presence || @all_ratings
+
+      saved_sort = session[:sort_by]
+
+      @sort_by =
+        if Movie::SORTABLE_COLUMNS.include?(saved_sort)
+          saved_sort
+        else
+          "title"
+        end
+    end
 
     @movies = Movie.with_ratings(@ratings_to_show, @sort_by)
   end
